5c049608e96a3609bc6e7bf709e8a3c5742905f3
AtomicFS <vojtech_vesely@white-hat-hacker.icu>
2024-09-14 10:31:20 +0200

    test: add linters
    Signed-off-by: AtomicFS <vojtech_vesely@white-hat-hacker.icu>

---
+++ .commitlint.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'always', 72],
+    'body-max-line-length': [2, 'always', 120],
+    'footer-max-line-length': [2, 'always', 120],
+  },
+}
--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package main implements the core logic
 package main
 
 func main() {
